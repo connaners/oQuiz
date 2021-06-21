9c382f02accc1c74e62d131f8120504aab15d6c7
connaners <subscriberohman@gmail.com>
2021-06-21 09:40:52 +0700

    select item

--- app/src/main/java/com/plete/oquiz/activity/NormalActivity.kt
+++ app/src/main/java/com/plete/oquiz/activity/NormalActivity.kt
@@ -1,7 +1,10 @@
 package com.plete.oquiz.activity
 
+import android.graphics.PorterDuff
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
+import android.widget.Toast
 import com.plete.oquiz.R
 import kotlinx.android.synthetic.main.activity_normal.*
 
@@ -13,6 +16,26 @@ class NormalActivity : AppCompatActivity() {
         btnBack.setOnClickListener {
             onBackPressed()
             finish()
+        }
+
+        var btnBg = 1
+
+        btn1.setOnClickListener {
+            if (btnBg == 1){
+                btn1.setBackgroundResource(R.drawable.ic_bahasa_focussed)
+                btnBg = 2
+            } else{
+                btn1.setBackgroundResource(R.drawable.ic_bahasa)
+                btnBg = 1
+            }
+        }
+
+        btnStart.setOnClickListener {
+            if (btnBg == 2){
+                Toast.makeText(this, "play", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this, "pilih mode", Toast.LENGTH_LONG).show()
+            }
         }
     }
 }
